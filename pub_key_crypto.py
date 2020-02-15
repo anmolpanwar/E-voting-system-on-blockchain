9bf0f47de6c9496075aeccace06215a886f03d97
Anmol Panwar <anmolpanwar8@gmail.com>
2020-02-15 15:59:28 +0530

    new

--- pub_key_crypto.py
+++ pub_key_crypto.py
@@ -1,6 +1,7 @@
 import Crypto
 from Crypto.PublicKey import RSA
 from Crypto import Random
+from Crypto.Cipher import PKCS1_OAEP
 from hashlib import sha256
 
 def genkey(keylen=1024):
@@ -16,5 +17,11 @@ bobPK = bobkey.publickey()
 
 print(bobPK)
 print(alicePK)
-secret = "abc"
+secret = "abc".encode()
 print("Message is:", secret)
+keyobj = PKCS1_OAEP.new(key = bobPK)
+bob = keyobj.encrypt(secret)
+print(bob)
+keyobj1 = PKCS1_OAEP.new(key = bobkey)
+dec = keyobj1.decrypt(bob)
+print(dec)
