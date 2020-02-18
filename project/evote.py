2bc593c1eb59a3f0110192d42d60f44d68b624df
Anmol Panwar <anmolpanwar8@gmail.com>
2020-02-19 02:36:45 +0530

    random

--- project/evote.py
+++ project/evote.py
@@ -2,25 +2,21 @@ from hashlib import *
 from cryptography.hazmat.primitives.asymmetric import *
 import cryptography
 from time import time
-from block import Block
-from flaskapp import *
-import csv
 
 difficulty = 2
 
 class vote:
-
     count = 0
 
     def __init__(self,candidateID):
         self.candidate = candidateID
         self.time = time()
         vote.count+=1
-        self.voteobject = {self.candidate:self.time}
-
+        Blockchain.votepool.append(self.__dict__)
 
 class Blockchain:
 
+    votepool = []
     chain = []
 
     @classmethod
@@ -28,13 +24,14 @@ class Blockchain:
         cls.length=len(cls.chain)
 
     def genesis(self):
-        gen = Block(0,"Let the real democracy rule!!", sha256(str("Let the real democracy rule!!").encode('utf-8')).hexdigest(), difficulty, time(),'')
+        gen = Block(0,"Let the real democracy rule!!", sha256(str("Let the real democracy rule!!").encode('utf-8')).hexdigest(), difficulty, time(),'0')
         return gen
 
     def addGenesis(self):
         Blockchain.chain.append(self.genesis())
 
     def display(self):
+
         for block in self.chain:
             print("Block Height: ", block.height)
             print("Data in block: ", block.data)
@@ -49,14 +46,14 @@ class Block:
 
     def __init__(self,height,data,merkleRoot,difficulty,timeStamp,prevHash):
         self.height = height                   #len(Blockchain.chain-1)
-        self.data = self.loadvote()                 #loadvote()
+        self.data = data                       #packdatainblock()
         self.merkleRoot = merkleRoot           #calculateMerkleRoot()
-        self.difficulty = difficulty           #cryptography difficulty
-        self.timeStamp = time()                #time()
+        self.difficulty = difficulty
+        self.timeStamp = timeStamp             #time()
         self.prevHash = prevHash               #Blockchain.chain[len(Blockchain.chain)-1].hash
-        self.nonce = self.pow()                #proof of work function will find nonce and hash will be found automatically. Return both values.
+        self.nonce = self.pow()                # proof of work function will find nonce and hash will be found automatically. Return both values.
 
-    def pow(self,zero=difficulty):             #proof-of-work method
+    def pow(self,zero=difficulty):
         self.nonce=0
         while(self.calcHash()[:zero]!='0'*zero):
             self.nonce+=1
@@ -68,27 +65,12 @@ class Block:
     def signvote(self):
         pass
 
-    @staticmethod
-    def loadvote():
-        votelist = []
-        try:
-            with open('votefile.csv', mode = 'r') as votepool:
-                csvreader = csv.reader(votepool)
-                for row in csvreader:
-                    votelist.append({'CandidateID':row[0], 'Time':row[1]})
-            return votelist
-
-        except(IOError,IndexError):
-            pass
-
-        finally:
-            print("done")
+    def loadvote(self):
+        pass
 
 
-if __name__ == '__main__':
+if __name__=='__main__':
 
     EVoting = Blockchain()
     EVoting.addGenesis()
     EVoting.display()
-    app.run()
-    print (Block.loadvote())
