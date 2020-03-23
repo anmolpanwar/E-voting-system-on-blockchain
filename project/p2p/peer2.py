eb38d78d823ddc8b2d08cca8c93d84a7d68595ae
Anmol Panwar <anmolpanwar8@gmail.com>
2020-03-23 14:55:36 +0530

    p2p testing

--- project/p2p/peer2.py
+++ project/p2p/peer2.py
@@ -1,8 +1,11 @@
 import socket
+while True:
+    try:
+        c = socket.socket()
+        c.connect(('192.168.0.152',9998))
+        name = input()
+        c.send(bytes(name,'utf-8'))
+        print(c.recv(1024).decode())
 
-c = socket.socket()
-c.connect(('localhost',9998))
-
-name = input()
-c.send(bytes(name,'utf-8'))
-print(c.recv(1024).decode())
+    except KeyboardInterrupt:
+        pass
