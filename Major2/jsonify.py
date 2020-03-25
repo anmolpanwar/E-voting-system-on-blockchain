a65eac4df6418aaafc4548a5edc7a7f969353ecd
Anmol Panwar <anmolpanwar8@gmail.com>
2020-03-25 21:56:46 +0530

    ..

--- Major2/jsonify.py
+++ Major2/jsonify.py
@@ -2,4 +2,7 @@ import simplejson as json
 
 def jsonify_votedata(votedatalist):
     jsondict = {'data':votedatalist[0],'key':votedatalist[1]}
+    #--this returns a string for the dictionary but converts the bytes data
+    #--into string...so we need to convert it back when recieved on the
+    #--other end of the peer....
     return json.dumps(jsondict)
