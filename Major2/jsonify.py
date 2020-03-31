f78b76048d68884dea0c82b71e25553ef0e842f5
Anmol Panwar <anmolpanwar8@gmail.com>
2020-03-31 13:09:04 +0530

    voter public key sent in json file to other peers

--- Major2/jsonify.py
+++ Major2/jsonify.py
@@ -1,11 +1,14 @@
 import simplejson as json
 
 def jsonify_votedata(votedatalist):
-    jsondict = {'data':votedatalist[0],'key':votedatalist[1]}
+    jsondict = {'voter_public_key':votedatalist[0],'data':votedatalist[1],'key':votedatalist[2]}
     #--this returns a string for the dictionary but converts the bytes data
     #--into string...so we need to convert it back when recieved on the
     #--other end of the peer....
     return json.dumps(jsondict)
+
+def reform_votedata():
+    pass
 
 if __name__=='__main__':
     ls = []
