f7c615ab715c8434738c19891044d09c87e9945b
Anmol Panwar <anmolpanwar8@gmail.com>
2020-04-02 20:42:40 +0530

    ..

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/anmolpanwar/Documents/PycharmProjects/python practice/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
