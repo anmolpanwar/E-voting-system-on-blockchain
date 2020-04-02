b57791015055ac909b2a5e1095dc5a51fc702f43
Anmol Panwar <anmolpanwar8@gmail.com>
2020-04-03 00:32:31 +0530

    verification methods in here. block verification. Blockchain verification

--- Major2/verification.py
+++ Major2/verification.py
@@ -1,16 +1,20 @@
 from hashlib import *
+from time import sleep
 
-def verify_blockchain(blockchain):
+def sync_blocks(blockchain):
     for i in range(1,len(blockchain)):
         if blockchain[i].prevHash == blockchain[i-1].calcHash():
             continue
         else:
-            error_msg ="""+-----------------------------------------+
-            |                                         |
-            | Somebody messed up at Block number - {} |
-            |                                         |
-            +-----------------------------------------+""".format(i)
+            return i, False
 
-            return error_msg, False
+    return 0, True
 
-    return True
+
+def verify_block(block):
+
+    check_1 = sha256((str(str(block.data)+str(block.nonce)+str(block.timeStamp)+str(block.prevHash))).encode('utf-8')).hexdigest()
+    sleep(5)
+    check_2 = sha256((str(str(block.data)+str(block.nonce)+str(block.timeStamp)+str(block.prevHash))).encode('utf-8')).hexdigest()
+
+    return check_1==check_2
