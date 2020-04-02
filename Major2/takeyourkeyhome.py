316d664d24ee19d25201d9451aaaeac5adbc495d
Anmol Panwar <anmolpanwar8@gmail.com>
2020-04-03 00:33:03 +0530

    QR code generated for voter to take their priv key home

---
+++ Major2/takeyourkeyhome.py
@@ -0,0 +1,22 @@
+import qrcode
+import pickle as pk
+
+def generate_QR(data,pin):
+    key = sk_to_data(data,pin)
+    qr = qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_L,box_size=10,border=4)
+    qr.add_data(key)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save('static/qr.jpg')
+
+def sk_to_data(privatekey,pin):
+    key = pk._dumps(privatekey)
+    key = str(key)[2:-1]
+    ls = [str(pin),key]
+    data = '****'.join(element for element in ls)
+    return data
+
+
+if __name__ == '__main__':
+    generate_QR('sdfvds','sdvbfv')
