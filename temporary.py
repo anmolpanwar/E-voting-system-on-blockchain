0642ccb14e76e19b5c79f976b2345105a216b4b5
Anmol Panwar <anmolpanwar8@gmail.com>
2020-04-03 00:31:51 +0530

    nothing important changed here

--- temporary.py
+++ temporary.py
@@ -21,7 +21,7 @@ keys['private'],keys['public'] = enc.rsakeys()
 keyobj = pk._dumps(keys['public'])
 print(keyobj)
 
-ls = [b'Cz6GPw+07hqqNnJ3YPHvU/0RlT4lhyXxEA3RUgOE8h1xqQFkUREkHC9NXvAvQ1RI2rbFu5t26jhfJuasgJ/APubIn8RyvmrAUfz0OUWjDaMsObk9/lF+ibNU8DPhEYv3beUnf233EBLTzYc2enYG1eX8UjBM4zghFbo73YD2rVawwAeT4W8n+miviCnrcDwgdnv/xBpISLK24PttEs+WKh5X/Rb4gjfSTrw+mG+Q97HYRxGupbK2a5XFMIl+Tm+m/jluBb1Ectf55Xe1VsNhofJdVlcUgZ5INC56MYyjsDlhYIN4W5jF1ImJMVkp5clJkwsC3ksGMEWsmB70v49q+gXZys8csCBph91Esrj4coZy29SUNeNy9siw2721QfaWLaXEJuVMO+MI2EYSm7xndZinPTR//SvtQkhTLZOwmb0igtleClgR7yjjrZztyU9dX6yp9b39nN1l6YO5ZUHr1eq5y0LEHHzkwMqsoq9zi1t+x1eyk8v84tAkaLFtJqLHoq/sBVLk4r4FXgS02vqdwFnvtyvW48HzHfwK50mqZSC2wAEuqDCUSFYRpTlClN4Y/bPeD4P8bWpuLorKtKMCpOjADO3IcQ1acrdGcdM+hqK4wl0vatHQSsf21MWkXvOnXtlNLpEs22wHu7z9r0+1f10OqZiLjqNKKNiFsYODq5BojrzSFsfy4bQ1a8euo+xg', b'rTYwNswB8+EJrq83hAF+rAaSkN6PLNCf6NaVsA4ag4HEl0vnc04H7mmR5Ari+7sCDajLsQI99ececbGD4f9flcr39C+KT6NMqvvu+bdIgQZnaIlgmb2cDswrrCra+jiSAbFfKeNIDAVInfyDn+WO72sS2PRmpTmGykx74Kz/HC4=']
+ls = [b'C6yOrzSFsfy4bQ172sS2PRmpTmGa8euo+xg', b'rTDAVInfyDn+WO72sS2PRmpTmGykx74Kz/HC4=']
 ls.append(str(keyobj)[2:-1])
 jsondict = {'pubkey':ls[2],'data':ls[0],'key':ls[1]}
 
