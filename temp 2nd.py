d7841caffed23395fbfbb32bc4da36f1260b624e
Anmol Panwar <anmolpanwar8@gmail.com>
2020-04-03 00:34:01 +0530

    ..

---
+++ temp 2nd.py
@@ -0,0 +1,9 @@
+def kuch():
+    error_msg ="""+-----------------------------------------+
+|                                         |
+| Somebody messed up at Block number - {} |
+|                                         |
++-----------------------------------------+""".format(22)
+    return error_msg
+
+print(kuch())
