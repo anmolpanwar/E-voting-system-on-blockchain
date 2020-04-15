f570846d338ea958327608a6af47caa8856b3dca
Anmol Panwar <anmolpanwar8@gmail.com>
2020-04-15 11:55:08 +0530

    temporary file to run experiments seperately

--- temp 2nd.py
+++ temp 2nd.py
@@ -1,9 +1,24 @@
-def kuch():
-    error_msg ="""+-----------------------------------------+
-|                                         |
-| Somebody messed up at Block number - {} |
-|                                         |
-+-----------------------------------------+""".format(22)
-    return error_msg
+n = int(input())
+m = int(input())
+movecount = 0
+chess = []
+piece = []
+for i in range(n):
+    row = list(map(str, input().strip()))
+    for j in row:
+        if j=='.':
+            continue
 
-print(kuch())
+        elif j=='Q':
+            piece.append({'name':'Q', 'x':j, 'y':i})
+
+        elif j=='B':
+            piece.append({'name':'B', 'x':j, 'y':i})
+
+        else:
+            piece.append({'name':'R', 'x':j, 'y':i})
+    chess.append(row)
+
+
+def queen(m,n,x,y,px,py):
+    while()
